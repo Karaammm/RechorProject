9fff826a1e84efeff105c8af943a4ac3d61d3315
Karaammm <140647957+Karaammm@users.noreply.github.com>
2025-02-21 21:37:28 +0100

    step2, part 3.1 complete

--- ReCHor/src/ch/epfl/rechor/journey/IcalBuilder.java
+++ ReCHor/src/ch/epfl/rechor/journey/IcalBuilder.java
@@ -2,6 +2,12 @@ package ch.epfl.rechor.journey;
 
 import java.security.cert.CRL;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.util.ArrayList;
+
+import ch.epfl.rechor.Preconditions;
 
 public final class IcalBuilder {
     public enum Component {
@@ -24,6 +30,7 @@ public final class IcalBuilder {
 
     private StringBuilder builder = new StringBuilder();
     private static final String CLRF = "\r\n";
+    private ArrayList<Component> components = new ArrayList<Component>();
 
     public IcalBuilder add(Name name, String value) {
         StringBuilder line = new StringBuilder(name.toString() + ":" + value);
@@ -41,7 +48,36 @@ public final class IcalBuilder {
         return this;
     }
 
-    public IcalBuilder(Name name, LocalDateTime dateTime) {
+    public IcalBuilder add(Name name, LocalDateTime dateTime) {
+        DateTimeFormatter line = new DateTimeFormatterBuilder()
+                .appendValue(ChronoField.YEAR, 4)
+                .appendValue(ChronoField.MONTH_OF_YEAR, 2)
+                .appendValue(ChronoField.DAY_OF_MONTH, 2)
+                .appendLiteral("T")
+                .appendValue(ChronoField.HOUR_OF_DAY, 2)
+                .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+                .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+                .toFormatter();
+        builder.append(line.format(dateTime));
 
+        return this;
+    }
+
+    public IcalBuilder begin(Component component) {
+        StringBuilder line = new StringBuilder("BEGIN" + component.name());
+        components.add(component);
+        builder.append(line + CLRF);
+        return this;
+    }
+
+    public IcalBuilder end() {
+        Preconditions.checkArgument(!components.isEmpty());
+        components.remove(components.size() - 1);
+        return this;
+    }
+
+    public String build() {
+        Preconditions.checkArgument(components.isEmpty());
+        return builder.toString();
     }
 }
---
+++ ReCHor/test/ch/epfl/rechor/journey/IcalBuilderTest.java
@@ -0,0 +1,5 @@
+package ch.epfl.rechor.journey;
+
+public class IcalBuilderTest {
+
+}
