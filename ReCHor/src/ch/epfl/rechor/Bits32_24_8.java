1ffc66978a91edfc12c4fbf081483cc652186134
ibrahim <ibrahim.khokher@epfl.ch>
2025-02-28 17:06:22 +0100

    docs for bits

--- ReCHor/src/ch/epfl/rechor/Bits32_24_8.java
+++ ReCHor/src/ch/epfl/rechor/Bits32_24_8.java
@@ -1,7 +1,20 @@
 package ch.epfl.rechor;
 
+/**
+ * Bit manipulation
+ *
+ * @author Karam Fakhouri (374510)
+ * @author Ibrahim Khokher(361860)
+ */
 public abstract class Bits32_24_8 {
 
+    /**
+     *
+     * @param bits24 24-bit vector
+     * @param bits8 8-bit vector
+     * @return returns the packed value of the bits
+     * @throws IllegalArgumentException if both bits are not the required length
+     */
     public static int pack(int bits24, int bits8) {
         Preconditions.checkArgument((bits24 >> 24) == 0);
         Preconditions.checkArgument((bits8 >> 8) == 0);
@@ -9,10 +22,20 @@ public abstract class Bits32_24_8 {
         return result;
     }
 
+    /**
+     *
+     * @param bits32 32-bit vector
+     * @return returns the value of the first 24 bits
+     */
     public static int unpack24(int bits32) {
         return bits32 >> 8;
     }
 
+    /**
+     *
+     * @param bits32 32-bit vector
+     * @return returns the value of the last 8 bits
+     */
     public static int unpack8(int bits32) {
         int mask = 255;
         return bits32 & mask;
