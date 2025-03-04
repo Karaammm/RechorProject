fc0aff647687d67e7814578c3c85fc671a6b0916
ibrahim <ibrahim.khokher@epfl.ch>
2025-03-04 08:57:06 +0100

    Tests for step 2 and the builder class

---
+++ ReCHor/src/ch/epfl/rechor/journey/ParetoFront.java
@@ -0,0 +1,38 @@
+package ch.epfl.rechor.journey;
+
+import java.util.NoSuchElementException;
+import java.util.function.LongConsumer;
+
+/**
+ * The Pareto Frontier
+ *
+ * @author Ibrahim Khokher(361860)
+ */
+public final class ParetoFront {
+
+    private final long[] packedCriteria;
+    ParetoFront EMPTY = new ParetoFront(new long[0]);
+
+    private ParetoFront(long[] packedCriteria){
+        this.packedCriteria = packedCriteria;
+    }
+
+    public int size(){
+        return packedCriteria.length;
+    }
+
+    public long get(int arrMins, int changes){
+        for(long l : packedCriteria){
+            long element = PackedCriteria.pack(arrMins, changes, PackedCriteria.payload(l));
+            if(element == l){
+                return l;
+            }
+        }
+        throw new NoSuchElementException();
+    }
+
+    public void forEach(LongConsumer action){
+        EMPTY.forEach(value -> action.accept(value));
+    }
+
+}
