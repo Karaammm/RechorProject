d87016b8b367162ac20017143259bf2f1c0d91c5
ibrahim <ibrahim.khokher@epfl.ch>
2025-03-05 05:43:25 +0100

    the builder class

--- ReCHor/src/ch/epfl/rechor/journey/ParetoFront.java
+++ ReCHor/src/ch/epfl/rechor/journey/ParetoFront.java
@@ -1,5 +1,6 @@
 package ch.epfl.rechor.journey;
 
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.function.LongConsumer;
 
@@ -36,8 +37,54 @@ public final class ParetoFront {
         paretoFront.forEach(value -> action.accept(value));
     }
 
-    public String toString(){
-        ParetoFront paretoFront = new ParetoFront(packedCriteria);
-        paretoFront.forEach(value -> System.out.println(value));
+//    public String toString(){
+//        ParetoFront paretoFront = new ParetoFront(packedCriteria);
+//        paretoFront.forEach(value -> String.valueOf(value));
+//
+//    }
+
+    public class Builder{
+        private long[] frontier;
+        private int size;
+
+        public Builder(){
+            this.frontier = new long[2];
+            this.size = frontier.length;
+        }
+
+        public Builder(Builder that){
+            this.frontier = Arrays.copyOf(that.frontier, that.size);
+            this.size = that.size;
+        }
+
+        public boolean isEmpty(){
+            return size == 0;
+        }
+
+        public Builder clear(){
+            size = 0;
+            return this;
+        }
+
+        public Builder add(long packedTuple){
+            int pos1 = 0;
+            int pos2 = 0;
+            boolean dominates = false;
+            for(long l : frontier){
+                pos1++;
+                if(PackedCriteria.dominatesOrIsEqual(packedTuple, l)){
+                    frontier[pos1] = packedTuple;
+                    dominates = true;
+//                    frontier = Arrays.copyOf(frontier, size + 1);
+//                    System.arraycopy(frontier, pos, frontier, pos + 1, size - pos);
+//                    frontier[pos] = packedTuple;
+                }
+            }
+            if(!dominates){
+
+            }
+            return this;
+        }
+
     }
 }
